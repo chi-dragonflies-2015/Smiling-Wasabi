79a7097efb2a59d9ba21a9bea71d901b252600a7
Ben Wootten <benmwootten@gmail.com>
2015-07-13 18:34:07 -0500

    rebases master onto films branch

--- spec/models/film_spec.rb
+++ spec/models/film_spec.rb
@@ -7,15 +7,23 @@ RSpec.describe Film, type: :model do
 
   before(:each) do
     DatabaseCleaner.start
+    @film = FactoryGirl.create(:film)
   end
 
   after(:each) do
     DatabaseCleaner.clean
   end
 
-  describe 'title' do
-    it 'is readable' do
-      expect(film.title).not_to be(nil)
-    end
+
+  it 'has a title' do
+    expect(@film.title).to eq("Zoolander")
+  end
+
+  it 'has a description' do
+    expect(@film.description).to eq("A funny film starring Ben Stiller as a male model.")
+  end
+
+  it 'has a link to movie art' do
+    expect(@film.movie_art).to eq('http://resizing.flixster.com/qLJGPymgNIMnjMyV5RAOyJp8pcI=/180x270/dkpu1ddg7pbsk.cloudfront.net/movie/11/16/89/11168983_ori.jpg')
   end
 end
