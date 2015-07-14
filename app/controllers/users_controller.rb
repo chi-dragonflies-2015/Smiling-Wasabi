4db6157408a8688ab614cd22f16eb4572f8bac7b
Amber Taniuchi <ambrela@gmail.com>
2015-07-13 19:59:12 -0500

    Add login to index and login routes/model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,11 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.create(user_params)
+    user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to root_path, notice: 'Account created successfully'
+      flash[:success] = "Account created successfully!"
+      redirect_to root_path
     else
       redirect_to signup_path
     end
@@ -17,6 +18,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password_digest)
+    params.require(:user).permit(:name, :email, :password)
   end
 end
