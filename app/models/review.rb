d5650ce22e4f9395edb0babbc4896a66c0322696
ac-adekunle <lola.adekunle@gmail.com>
2015-07-14 13:57:48 -0500

    update review model validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,7 @@ class Review < ActiveRecord::Base
   belongs_to :user
 
   validates :content, presence: true
-  validates :rating, presence: true
+  validates :rating, :inclusion => { :in => [true,false]}
   validates_associated :film, :user
 
 end
