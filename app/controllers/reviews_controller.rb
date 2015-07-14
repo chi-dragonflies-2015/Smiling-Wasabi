e34c86183720944ecd3f3dba5d9836c198622402
ac-adekunle <lola.adekunle@gmail.com>
2015-07-14 13:58:58 -0500

    update review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,33 @@
 class ReviewsController < ApplicationController
 
   def index
+  	@film = Film.find(params: :film_id)
+  	@reviews = @film.reviews
   end
 
+  def new
+  	#create a new review
+  	#shows form
+  end
+
+  def create
+  end
+
+  def show
+  	#individual review
+  end
+
+  def edit
+  	#edit review form
+  	#only avaiable to creator
+  end
+
+  def update
+  	#updates review
+  end
+
+  def destroy
+  	#deletes review
+  end
 
 end
