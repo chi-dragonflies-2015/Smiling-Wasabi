89a41157568f92893d71eff0c4c238bf3e38de15
ac-adekunle <lola.adekunle@gmail.com>
2015-07-14 15:29:22 -0500

    update config routes to be more explicit

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,9 @@ Rails.application.routes.draw do
   get '/films/:film_id/reviews' => 'reviews#index'
   get '/films/:film_id/reviews/new' => 'reviews#new'
   post '/films/:film_id/reviews' => 'reviews#create'
-  get '/films/:film_id/reviews/:id' => 'reviews#show'
-  get '/films/:film_id/reviews/:id/edit' => 'reviews#edit'
-  put '/films/:film_id/reviews/:id' => 'reviews#update'
-  delete '/films/:film_id/reviews/:id' => 'reviews#destroy'
+  get '/films/:film_id/reviews/:review_id' => 'reviews#show'
+  get '/films/:film_id/reviews/:review_id/edit' => 'reviews#edit'
+  put '/films/:film_id/reviews/:review_id' => 'reviews#update'
+  delete '/films/:film_id/reviews/:review_id' => 'reviews#destroy'
 
 end
