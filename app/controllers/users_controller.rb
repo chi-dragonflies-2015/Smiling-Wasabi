c349352935ed87a4bc1063054c5e3b0e8a334b1f
Amber Taniuchi <ambrela@gmail.com>
2015-07-13 19:30:39 -0500

    Add session helpers and routes for login/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     user = User.create(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to root_path
+      redirect_to root_path, notice: 'Account created successfully'
     else
       redirect_to signup_path
     end
