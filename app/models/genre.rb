d037ee24cc089db2c88194115d8e1b40e8587d81
Ben Wootten <benmwootten@gmail.com>
2015-07-14 14:32:22 -0500

    adds very basic genre#index

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,7 +3,7 @@ class Genre < ActiveRecord::Base
   has_many :films, through: :films_genres
 
   def top5films
-    sorted_films = films.sort{ |film1, film2| film1.review_score <=> film2.review_score }
+    sorted_films = films.sort{ |film1, film2| film2.review_score <=> film1.review_score }
     sorted_films.first(5)
   end
 end
