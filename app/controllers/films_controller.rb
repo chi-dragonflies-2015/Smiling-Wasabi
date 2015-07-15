253df96f84efe4a53e0be6323fea3761490b98fa
Amber Taniuchi <ambrela@gmail.com>
2015-07-15 16:43:02 -0500

    Add film show page with main card and reviews

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,9 +1,10 @@
 class FilmsController < ApplicationController
   def index
-    @films = Film.top_movies
+    @films = Film.all
   end
 
   def show
-    @film = Film.movie_title()
+    @film = Film.find_by(params[:film_id])
+    @reviews = @film.reviews
   end
 end
