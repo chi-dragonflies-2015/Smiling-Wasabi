69c4f8a2d40cdc71ac73c1790a26869e8da23519
ac-adekunle <lola.adekunle@gmail.com>
2015-07-15 17:29:15 -0500

    seeded

--- db/schema.rb
+++ db/schema.rb
@@ -63,7 +63,6 @@ ActiveRecord::Schema.define(version: 20150715155601) do
     t.string   "email"
     t.string   "password_digest"
     t.string   "role"
-    t.string   "avatar"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
