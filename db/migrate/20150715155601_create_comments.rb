ef28b59dfae5e6e9347663442d767e47ca731e37
ac-adekunle <lola.adekunle@gmail.com>
2015-07-15 11:13:31 -0500

    add comments table and migration

--- db/migrate/20150715155601_create_comments.rb
+++ db/migrate/20150715155601_create_comments.rb
@@ -1,11 +1,12 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.string :comment_type
+      t.text :content
+      t.boolean :rating
+      t.references :user
       t.references :commentable, polymorphic: true, index: true
-      t.references :review
-      t.references :film
 
+      t.timestamps
     end
   end
 end
