cdf9e4db83a01fe66fa2049df9e33980edbbba15
Ben Wootten <benmwootten@gmail.com>
2015-07-15 18:24:58 -0500

    debugs the vote controller and sets up votes on reviews on the films#show page

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -3,22 +3,25 @@ $(document).ready(function(){
 });
 
 function bindVotes(){
-  $('.upvote').on('click', function(){ vote(event, true).bind(this) });
-  $('.downvote').on('click', function(){ vote(event, false).bind(this) });
+  $('.upvote').on('click', function(event){ vote(event, true) });
+  $('.downvote').on('click', function(event){ vote(event, false) });
 }
 
 function vote(event, vote){
   event.preventDefault();
-  var that = this
-  var href = this.attr('href');
+  var that = event.target
+  var href = $(that).attr('href');
   $.ajax({
     url: href,
-    method: post,
+    method: 'POST',
     data: { vote: vote },
-    dataType: json
-  }).done(function(response){
-    that.toggleClass('voted')
-    $(that).siblings().removeClass('voted')
-    $(that).siblings('score').val(response)
+    dataType: 'json',
+    statusCode:{
+      200: function(response){
+          $(that).toggleClass('voted');
+          $(that).siblings().removeClass('voted');
+          $(that).siblings('.score').text(response.score + '% with ' + response.num_votes + ' people voting');
+        }
+    }
   });
-}
+};
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,23 +1,30 @@
 class VotesController < ApplicationController
 
   def vote
-    voted_object = Review.find(params[:review_id]) if params[:review_id]
-    voted_object = Film.find(params[:film_id]) if params[:film_id]
-    vote = voted_object.votes.find_by(voter_id: current_user.id)
-    
-    if vote && vote.value == params[:vote]
-      vote.destroy
-    elsif vote
-      vote.destroy
-      voted_object.votes.create(voter: current_user, value: params[:vote])
+    if current_user.nil?
+      render :json => { :status => 403 }
     else
-      voted_object.votes.create(voter: current_user, value: params[:vote])
-    end
+      voted_object = Review.find(params[:review_id]) if params[:review_id]
+      voted_object = Film.find(params[:film_id]) if params[:film_id]
+      vote = voted_object.votes.find_by(voter_id: current_user.id)
 
-    if request.xhr?
-      voted_object.user_score 
-    else
-      redirect_to :back
+      if vote && vote.value.to_s == params[:vote]
+        vote.destroy
+        puts 1
+      elsif vote
+        puts 2
+        vote.destroy
+        voted_object.votes.create(voter: current_user, value: params[:vote])
+      else
+        puts 3
+        voted_object.votes.create(voter: current_user, value: params[:vote])
+      end
+
+      if request.xhr?
+        render :json => { status: :ok, score: voted_object.user_score.to_json, num_votes: voted_object.votes.length }
+      else
+        redirect_to :back
+      end
     end
   end
 end
