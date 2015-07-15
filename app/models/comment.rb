3b2242b9c235971213a77f50e82b5748b863839c
ac-adekunle <lola.adekunle@gmail.com>
2015-07-15 16:41:24 -0500

    create pending controller tests

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :commentable, polymorphic: true
   belongs_to :user
+
+  validates :content, presence: true
+  validates :rating, inclusion: { in: [true,false] }
 end
