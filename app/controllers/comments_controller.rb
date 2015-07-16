a87aafb5ea79d9a9aa1b1c46ff45dbcdc3fcd336
ac-adekunle <lola.adekunle@gmail.com>
2015-07-15 21:20:17 -0500

    create film comment works

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,9 +15,8 @@
   end
 
   def create
-    puts '----------------------------------------'
     @film = Film.find(params[:film_id]) 
-
+  
     if params[:review_id]
       @review = Review.find(params[:review_id]) 
       @comment = @review.comments.new(comment_params)
@@ -28,13 +27,13 @@
     
     else
       @comment = @film.comments.new(comment_params)
+      @comment.user = current_user
       if @comment.save
         redirect_to @film, notice: 'Comment was successfully created.'
       end 
     end
 
     unless @comment.save
-      puts '***************************************************'
       render :new
     end
   end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,5 @@ class Comment < ActiveRecord::Base
   belongs_to :user
 
   validates :content, presence: true
-  validates :rating, inclusion: { in: [true,false] }
+
 end
